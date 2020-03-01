2dfae4fadb336047035a1066981f3d9ed1a6b18f
yarik <yariks@i.ua>
2020-03-01 11:24:44 +0200

    Initial commit

---
+++ src/com/company/myThread.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class myThread extends Thread {
+    @Override
+    public void run() {
+        Thread thread = Thread.currentThread();
+        for (int i = 0; i <10000 ; i++) {
+            System.out.println("Name - " +thread.getName()+" "+i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+}
